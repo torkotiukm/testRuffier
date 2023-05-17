102fd98f7513ec8b045abacd5167ed96c8103f10
torkotiukm <maksymtorkotiuk@gmail.com>
2023-05-17 17:48:25 +0100

    v1.0

--- my_app.py
+++ my_app.py
@@ -1,1 +1,10 @@
-print("")
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget,
+        QHBoxLayout, QVBoxLayout,
+        QPushButton, QLabel, QLineEdit)
+
+app = QApplication([])
+window = QWidget()
+window.show()
+app.exec_()
